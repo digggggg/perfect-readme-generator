785111c527b1a6a2fa1f054666a6911a95c9262d
Jondo <chet9880@gmail.com>
2022-07-16 09:13:13 -0400

    filled questions array

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
-// TODO: Include packages needed for this application
+
 const inquirer = require("inquirer")
-// TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {
+    type: 'input',
+    message: 'What is your Project Title?',
+    name: 'title',
+    },
+    {
+    type: 'input',
+    message: 'Describe your project...',
+    name: 'description',
+    },
+    {
+    type: 'input',
+    message: 'How to install your project?',
+    name: 'installation',
+    },
+    {
+    type: 'input',
+    message: 'How do you use your project?',
+    name: 'usage',
+    },
+    {
+    type: 'input',
+    message: 'Who contributed to your project?',
+    name: 'contributing',
+    },
+    {
+    type: 'input',
+    message: 'Write some test cases for your project...',
+    name: 'tests',
+    },
+    {
+    type: 'list',
+    message: 'Pick a license for your project...',
+    name: 'license',
+    },
+    {
+    type: 'input',
+    message: 'What is your github username?',
+    name: 'git-username',
+    },
+    {
+    type: 'input',
+    message: 'What is your email?',
+    name: 'email',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
